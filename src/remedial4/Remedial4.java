0817dbd870d37fe425e204b723f72ea45fcf5bc6
Arya Gading Indratama <arya_gading_27rpl@student.smktelkom-mlg.sch.id>
2019-01-31 16:42:13 +0700

    Remedial 4

---
+++ src/remedial4/Remedial4.java
@@ -0,0 +1,17 @@
+package remedial4;
+
+public class Remedial4 {
+
+        public static void main(String[] args) {
+            
+        for (int i = 3; i <= 100; i+= 10) {
+            System.out.print(i);
+            System.out.println(",");
+        }
+            int i = 10;
+            
+        
+        System.out.println("Banyak Angka 3 adalah "+ i );
+    }
+    
+}
